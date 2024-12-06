bbb5f892ecf51480fcc54ce8e10332ee6cc4cdd7
234792 <234792@KB37-309-C02>
2024-12-06 11:24:40 +0300

    Добавьте файлы проекта.

---
+++ friend_function/friend_function.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Person
+{
+	string fio;
+public:
+	Person(string fio)
+};
+class Car
+{
+private:
+	string marka, model;
+public: 
+	Car(string marka, string  model)
+	{
+		this->marka = marka; this->model = model;
+	}
+	friend void _friend(Car c, string fio);
+	
+};
+ void _friend(Car c, string fio)
+{
+	cout << fio << " угнал автомобиль Лейсанушки "
+		<< c.marka << " " << c.model << endl;
+}
+ int main()
+ {
+	 setlocale(LC_ALL, "Russian");
+	 Car bmw{ "BMW", "316i" };
+	 Car audi{ "Audi", "RS7" };
+	 _friend(bmw, "Иван Заволокин");
+	 _friend(audi, "Руслан Кушманцев");
+
+ }
